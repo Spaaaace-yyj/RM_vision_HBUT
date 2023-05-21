8db96393f3534585205737edd3b46f99608aadbf
lihuagit <3190995951@qq.com>
2023-05-21 23:41:57 +0800

    参数文件更新

--- armor_debug.sh
+++ armor_debug.sh
@@ -1,2 +1,5 @@
+source /opt/ros/galactic/setup.bash
 source install/setup.bash
-ros2 launch bringup armor_launch.py
+export ROS_LOCALHOST_ONLY=1
+ros2 launch bringup armor_launch.py use_serial:=True
+# ros2 launch bringup trt_armor_launch.py  use_serial:=True
